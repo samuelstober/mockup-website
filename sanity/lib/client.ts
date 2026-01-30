916cff00d2eef1901b7c79caab2c498576df069a
Samuel Stober <samuelstober@outlook.com>
2026-01-30 14:12:20 +0100

    fix: use server-safe sanity env variables

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,8 +1,14 @@
 import { createClient } from 'next-sanity'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId:
+    process.env.SANITY_PROJECT_ID ||
+    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+
+  dataset:
+    process.env.SANITY_DATASET ||
+    process.env.NEXT_PUBLIC_SANITY_DATASET,
+
   apiVersion: '2024-01-01',
   useCdn: true,
 })
