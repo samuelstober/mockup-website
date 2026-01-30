5b2b61b748d49a13fc55b1463312445ef4e94240
Samuel Stober <samuelstober@outlook.com>
2026-01-30 14:16:09 +0100

    fix: enforce sanity env vars

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,14 +1,20 @@
 import { createClient } from 'next-sanity'
 
+const projectId =
+  process.env.SANITY_PROJECT_ID ??
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+
+const dataset =
+  process.env.SANITY_DATASET ??
+  process.env.NEXT_PUBLIC_SANITY_DATASET
+
+if (!projectId || !dataset) {
+  throw new Error('Sanity environment variables are missing')
+}
+
 export const client = createClient({
-  projectId:
-    process.env.SANITY_PROJECT_ID ||
-    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-
-  dataset:
-    process.env.SANITY_DATASET ||
-    process.env.NEXT_PUBLIC_SANITY_DATASET,
-
+  projectId,
+  dataset,
   apiVersion: '2024-01-01',
-  useCdn: true,
+  useCdn: false,
 })
